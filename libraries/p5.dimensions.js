ea2f648bc86fdb43d4937abe59b02fd4d4e1231a
Max Segal <max.j.segal@gmail.com>
2017-01-14 11:56:39 -0500

    Added divide vector function

--- libraries/p5.dimensions.js
+++ libraries/p5.dimensions.js
@@ -87,6 +87,15 @@ p5.prototype.nMul = function(v, n){ //Multiplies vector's values
     return output; //Returns vector
 }
 
+p5.prototype.nDiv = function(v, n){ //Divides vector's values
+    var output = new nVector();
+    values = Object.keys(v).map(function(k) { return v[k] });
+    for (var i = 0; i < values.length; i++){
+        output[Object.keys(v)[i]] = values[i]/n;
+    }
+    return output;
+}
+
 p5.prototype.nMag = function(v1) {
     //implement magnitude calculation of nVectors
     var dimensionCount = Object.keys(v1).length;

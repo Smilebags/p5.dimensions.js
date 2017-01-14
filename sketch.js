c616bf78afa2b0b72157b7d5aa7b63540d8e6fd0
Smilebags <christopher@datawise.com.au>
2017-01-14 16:38:13 +1100

    Created a working example

--- sketch.js
+++ sketch.js
@@ -1,17 +1,31 @@
+var distance;
+var values;
+var outputElement = document.getElementById("distanceLabel");
+
+
+
 function setup() {
-    createCanvas(400,400, WEBGL);
-
-
-    var point1 = nVector(0,0,0,0);
-    console.log(typeof(point1));
-    console.log(point1);
-    var point2 = nVector(1,1,1,1);
-    console.log(typeof(point2));
-    console.log(point2);
-        var distance = nDist(point1,point2);
-    console.log(distance);
+    calculate();
 }
 
 function draw() {
+    calculate();
+}
 
+function calculate() {
+    var a = parseFloat(document.getElementById("dimIn1").value);
+    var b = parseFloat(document.getElementById("dimIn2").value);
+    var c = parseFloat(document.getElementById("dimIn3").value);
+    var d = parseFloat(document.getElementById("dimIn4").value);
+    var e = parseFloat(document.getElementById("dimIn5").value);
+    var f = parseFloat(document.getElementById("dimIn6").value);
+    var g = parseFloat(document.getElementById("dimIn7").value);
+    var h = parseFloat(document.getElementById("dimIn8").value);
+    values = {
+        pos1: nVector(a,b,c,d),
+        pos2: nVector(e,f,g,h),
+        result: ''
+    };
+    values.result = nDist(values.pos1,values.pos2);
+    outputElement.innerHTML = values.result;
 }

c566f26f1548e09599507de0d4f380734f5c72c0
Smilebags <christopher@datawise.com.au>
2017-01-14 15:42:05 +1100

    Added functionality for pos1.nDist(pos2) use.
    Added comments and removed unnessecary debugging comments.

--- libraries/p5.dimensions.js
+++ libraries/p5.dimensions.js
@@ -1,19 +1,25 @@
-console.log("p5.dimensions working");
+
 
 p5.prototype.nDist = function (pos1, pos2) {
-    var positions = [];
-    a1 = Object.keys(pos1).map(function(k) { return pos1[k] }); //turns pos1's values into an array
-    a2 = Object.keys(pos2).map(function(k) { return pos2[k] }); //turns pos2's values into an array
-    positions = a1.concat(a2) //Joins arrays together
-    var total = 0;
-    for(var i=0; i < positions.length/2; i++){
-        total += Math.pow(positions[i]-positions[i+positions.length/2], 2);
+    //nDist calculates the euclidean distance between two points(nVector objects), or between 'this' and another point. 
+    if (arguments.length == 1) {
+        return nDist(this,pos1);
+    } else {
+        var positions = [];
+        a1 = Object.keys(pos1).map(function(k) { return pos1[k] }); //turns pos1's values into an array
+        a2 = Object.keys(pos2).map(function(k) { return pos2[k] }); //turns pos2's values into an array
+        positions = a1.concat(a2) //Joins arrays together
+        var total = 0;
+        for(var i=0; i < positions.length/2; i++){
+            total += Math.pow(positions[i]-positions[i+positions.length/2], 2);
+        }
+        return Math.sqrt(total);
     }
-    return Math.sqrt(total);
 };
 
 
 p5.prototype.nVector = function() {
+    //nVector is a constructor function which creates an object with x,y,z, then alphabetically named properties.
     var obj = new Object();
     var dimensionalSymbols = ["x","y","z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w",
     "xx","yy","zz","aa","bb","cc","dd","ee","ff","gg","hh","ii","jj","kk","ll","mm","nn","oo","pp","qq","rr","ss","tt","uu","vv","ww"];
